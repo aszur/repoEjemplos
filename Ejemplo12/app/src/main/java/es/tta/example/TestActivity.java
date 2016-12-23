735d5777540ee8aa201a0569ed838e1534483eed
Asier Unanue <aunanue001@gmail.com>
2016-12-23 13:10:02 +0100

    Logica del test añadida.

--- Ejemplo12/app/src/main/java/es/tta/example/TestActivity.java
+++ Ejemplo12/app/src/main/java/es/tta/example/TestActivity.java
@@ -2,6 +2,9 @@ package es.tta.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.docencia.ejemplo1.R;
@@ -16,8 +19,21 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        Test test = data.getTest();
-        TextView preguntaTest = (TextView)findViewById(R.id.preguntaTest);
-        preguntaTest.setText();
+        String[] answer ={"answer1","answer2","answer3","answer4","answer5"};
+        RadioGroup opciones = (RadioGroup) findViewById(R.id.opcionesTest);
+        int i=0;
+        for (String opcion : answer) {
+            RadioButton radio = new RadioButton(this);
+            radio.setText(opcion);
+            radio.setId(i);
+            radio.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    findViewById(R.id.botonEnviarRespuestaTest).setVisibility(View.VISIBLE);
+                }
+            });
+            opciones.addView(radio);
+            i++;
+        }
     }
 }
