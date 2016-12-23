3ff1dd8ab7aeab9e3afc22c066fa587b74f47f91
Asier Unanue <aunanue001@gmail.com>
2016-12-23 12:51:09 +0100

    Logica del MenuActivity añadida.

--- Ejemplo12/app/src/main/java/es/tta/example/MenuActivity.java
+++ Ejemplo12/app/src/main/java/es/tta/example/MenuActivity.java
@@ -7,26 +7,51 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.docencia.ejemplo1.R;
 
 public class MenuActivity extends AppCompatActivity {
-
+    Button botonEjercicio;
+    Button botonSeguimiento;
     public final static String EXTRA_LOGIN = "es.tta.ejemplo.login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         Intent intent = getIntent();
+        botonEjercicio = (Button)findViewById(R.id.botonEjercicio);
+        botonSeguimiento = (Button)findViewById(R.id.botonSeguimiento);
+        botonEjercicio.setOnClickListener(toOption);
+        botonSeguimiento.setOnClickListener(toOption);
         /*Esto de pasar datos se podría hacer mediante una interfaz? ¿Como?*/
         Bundle bundle = intent.getExtras();
         String usuario = bundle.getString(EXTRA_LOGIN);
-        TextView textLogin = (TextView)findViewById(R.id.bienvenidaUsuario);
+        TextView textLogin = (TextView) findViewById(R.id.bienvenidaUsuario);
         textLogin.setText(R.string.bienvenida + usuario + R.string.bienvenida2);
 
+    }
+    //Funcion llamada desde layout
+    public void test(View v){
+        Intent intentTest = new Intent(this, TestActivity.class);
+        intentTest.putExtra("usuario", EXTRA_LOGIN); //Se pasa el login por si hay que subir la respuesta asociada al usuario
+        startActivity(intentTest);
+    }
+    //Funcion llamada desde un listener
+    View.OnClickListener toOption = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.botonEjercicio){
+                Intent intent = new Intent(MenuActivity.this, ExerciseActivity.class);
+                startActivity(intent);
+            }else if(v.getId() == R.id.botonSeguimiento){
+                //Intent a Seguimiento
+            }
 
 
-   }
+        }
+    };
+
 
 }
