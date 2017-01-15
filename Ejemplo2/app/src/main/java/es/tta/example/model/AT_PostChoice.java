a0d9fe843db0d52b10f791ff248266e1b086cb63
Asier Unanue <aunanue001@gmail.com>
2017-01-15 16:40:42 +0100

    PostChoice added.

---
+++ Ejemplo2/app/src/main/java/es/tta/example/model/AT_PostChoice.java
@@ -0,0 +1,34 @@
+package es.tta.example.model;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by asier on 13/01/17.
+ */
+
+public class AT_PostChoice extends AsyncTask<String, Integer, Void> {
+    @Override
+    protected Void doInBackground(String... opcion) {
+        String surl = "http://u017633.ehu.eus:28080/ServidorTta/rest/tta/postChoice";
+        try {
+            RestClient.setHttpBasicAuth("12345678A", "tta");
+            JSONObject jo = new JSONObject();
+            jo.put("userId", opcion[0]);
+            jo.put("choiceId", opcion[1]);
+            RestClient.postJson(jo, "surl");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
+
